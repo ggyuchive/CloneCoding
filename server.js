80f74168767aa4deeeced8d1a72c474a6887cb75
ggyuchive <zarami12140@naver.com>
2022-10-15 00:29:29 +0900

    [add] server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+// hellonode1.js
+var server = require('http');
+server.createServer(function(req, res){
+  res.writeHead(200, { 'Content-Type' : 'text/plain' });
+  res.end("Hello node.js! \n");
+}).listen(4000, 'localhost');
+
+console.log('Server is running at http://localhost:4000/');
