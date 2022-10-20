53f20ef5ad37695671f110f15c2eb1a6f7fc1867
ggyuchive <zarami1214@g.skku.edu>
2022-10-20 19:47:31 +0900

    [add] connect body-parser, postman

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {User} = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+//application/json
+app.use(express.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ByeonggyuPark:76031628@clonecoding.pfohjjv.mongodb.net/?retryWrites=true&w=majority')
@@ -8,4 +14,17 @@ mongoose.connect('mongodb+srv://ByeonggyuPark:76031628@clonecoding.pfohjjv.mongo
 .catch(err => console.log(err))
 
 app.get('/', (req,res) => res.send('Hello World!'))
+
+app.post('/register', (req,res) => {
+    // 회원가입 할 때 필요한 정보들을 client에서 가져오면 db에 넣어줌
+    
+    const user = new User(req.body)
+    user.save((err,userInfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 app.listen(port,() => console.log(`Example app listening on port ${port}!`))
